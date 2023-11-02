15f75c7d0bf4a9b1a7b9fe6f820d9ea1deda24cb
BlackRose <tungth@hilo.com.vn>
2023-11-02 17:40:52 +0700

    update

--- lib/constant/application_constant.dart
+++ lib/constant/application_constant.dart
@@ -2,5 +2,5 @@ class ApplicationConstant {
   static const String EMPTY = "";
   static const String USERNAME = "USERNAME";
   static const String USERPASSWORD = "USERPASSWORD";
-
+  static const String URL_NEWS = "https://i1-kinhdoanh.vnecdn.net/2023/11/02/fed1-2023-11-02t161657-025-169-7434-6504-1698917034.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=GNQB8GFL2zQyoJOZjePMrQ";
 }
